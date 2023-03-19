b3e582aa2d721cfecacabbdd28ecb2e9b59558dd
estebanrm1 <solsel90@gmail.com>
2023-03-19 01:07:47 -0300

    Se resolvio el ejercicio 2

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,9 +1,9 @@
 let meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
 
-document.write(`<h2>Lista de meses</h2>`)
-document.write('<ul>')
+document.write(`<h2>Lista de meses</h2>`);
+document.write('<ul>');
 for(let indice = 0; indice < meses.length ; indice++ ){
     document.write(`<li>${meses[indice] } </li>`)
 }
-document.write('</ul>')
+document.write('</ul>');
