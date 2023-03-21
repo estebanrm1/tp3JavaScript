3e8445a78c721ffc5b5b130c1c0d323046a8506f
estebanrm1 <solsel90@gmail.com>
2023-03-21 01:03:50 -0300

    Se resolvio el ejercicio 7 y 6

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,7 +1,7 @@
 function esParoImpar(numero){
     if(numero % 2 == 0){
-        return(`El numero ${numero} es Par`);
-        }else{
+        return(`El numero ${numero} es par`);
+    }else{
         return(`El numero ${numero} es impar`);
     }
 }
